454f5b87982eb64fc8e13b6ec41215215d362ead
mirkoterzic <mirko.terzic2002@gmail.com>
2024-08-21 12:07:58 +0200

    Setup Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import { useState } from "react";
 import { data } from "./data";
+import logo from "./images/logo.svg";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import avatar from "./images/image-avatar.png";
+function Header() {
+  return (
+    <>
+      <header className="flex  items-center justify-between p-8 border-b border-slate-400 max-w-7xl mx-auto">
+        <div className="flex  items-center justify-start gap-4">
+          <img src={logo}></img>
+          <nav>
+            <ul className="flex  items-center justify-start gap-4">
+              <li>Collection</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <ul className="flex  items-center justify-start gap-4">
+            <li>
+              <button>
+                <MdOutlineShoppingCart />
+              </button>
+            </li>
+            <li>
+              <img src={avatar} alt="avatar image" className="w-12" />
+            </li>
+          </ul>
+        </div>
+      </header>
+    </>
+  );
+}
 function App() {
   const [products] = useState(data);
   return (
     <>
-      <h1>E Commerce Product Page</h1>
+      <Header />
     </>
   );
 }
