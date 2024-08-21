89d3ed5617da5326333fa9fb613acdf7d1de6874
mirkoterzic <mirko.terzic2002@gmail.com>
2024-08-21 14:22:53 +0200

    added on mobile ability to scroll images also tinkered responsivnes for info section

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 import avatar from "./images/image-avatar.png";
 import minus from "./images/icon-minus.svg";
 import plus from "./images/icon-plus.svg";
+
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 function Header() {
   return (
     <>
       <header className="flex  items-center justify-between p-8 border-b border-slate-400 max-w-7xl mx-auto">
         <div className="flex  items-center justify-start gap-4">
           <img src={logo}></img>
-          <nav>
+          <nav className="hidden">
             <ul className="flex  items-center justify-start gap-4">
               <li>Collection</li>
               <li>Men</li>
@@ -25,7 +27,7 @@ function Header() {
           <ul className="flex  items-center justify-start gap-4">
             <li>
               <button>
-                <MdOutlineShoppingCart />
+                <MdOutlineShoppingCart className=" text-2xl text-slate-400" />
               </button>
             </li>
             <li>
@@ -41,7 +43,24 @@ function App() {
   const [products] = useState(data);
   const [value, setValue] = useState(0);
   const [amount, setAmout] = useState(0);
+  const [slideIndex, setSlideIndex] = useState(1);
+
   const { mainImage } = products[value];
+
+  const nextSlide = () => {
+    if (slideIndex !== products.length) {
+      setSlideIndex(slideIndex + 1);
+    } else if (slideIndex === products.length) {
+      setSlideIndex(1);
+    }
+  };
+  const previousSlide = () => {
+    if (slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1);
+    } else if (slideIndex === 1) {
+      setSlideIndex(products.length);
+    }
+  };
 
   const handleMinus = () => {
     setAmout(amount - 1);
@@ -50,11 +69,42 @@ function App() {
   return (
     <>
       <Header />
-      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10">
+      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 lg:place-items-center lg:py-20 ">
         <article>
-          <img src={mainImage} alt="" className="w-full rounded-2xl" />
+          <div>
+            {products.map((item, index) => (
+              <div
+                key={item.id}
+                className={slideIndex === index + 1 ? "relative" : "hidden"}
+              >
+                <img
+                  src={item.mainImage}
+                  alt=""
+                  className="w-full lg:rounded-2xl"
+                />
+                <ul className="lg:hidden">
+                  <li>
+                    <button
+                      onClick={previousSlide}
+                      className=" bg-white rounded-full p-5 shadow absolute left-4 top-1/2 -translate-y-1/2"
+                    >
+                      <FaChevronLeft />
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      onClick={nextSlide}
+                      className=" bg-white rounded-full p-5 shadow absolute right-4 top-1/2 -translate-y-1/2 "
+                    >
+                      <FaChevronRight />
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            ))}
+          </div>
 
-          <ul className="flex items-center justify-start gap-5 flex-wrap mt-5">
+          <ul className="hidden lg:flex items-center justify-start gap-5 flex-wrap mt-5">
             {products.map((item, index) => (
               <li
                 key={item.id}
@@ -80,7 +130,7 @@ function App() {
             Featuring a durable rubber outer sole, they’ll withstand everything
             the weather can offer.
           </p>
-          <div className="flex flex-wrap items-center justify-between ">
+          <div className="flex flex-wrap items-center justify-between lg:flex-col lg:items-start lg:gap-2 ">
             <ul className="flex items-center gap-5">
               <li className="text-slate-900 font-bold text-2xl">$125.00</li>
               <li className="bg-orange-100 py-1 px-2 text-orange-400  tracking-wide text-sm font-bold inline-block rounded shadow ">
@@ -91,8 +141,8 @@ function App() {
               <s>$250.00</s>
             </p>
           </div>
-          <div className="mt-10 ">
-            <ul className=" flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow">
+          <div className="mt-10 lg:flex items-center justify-between gap-2">
+            <ul className=" flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow lg:flex-1 ">
               <li onClick={handleMinus} className="cursor-pointer">
                 <img src={minus} alt="" />
               </li>
@@ -104,8 +154,8 @@ function App() {
                 <img src={plus} alt="" />
               </li>
             </ul>
-            <div>
-              <button className="flex items-center justify-center gap-4 bg-orange-500  py-2 px4 text-white font-bold rounded-lg shadow mt-5 w-full ">
+            <div className="lg:flex-1">
+              <button className="flex items-center justify-center gap-4 bg-orange-500  py-2 px4 text-white font-bold rounded-lg shadow mt-5 w-full lg:mt-0 hover:bg-orange-600 transition-all duration-200 ">
                 <MdOutlineShoppingCart /> Add to cart
               </button>
             </div>
